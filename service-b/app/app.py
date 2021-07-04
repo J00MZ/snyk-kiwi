fb39e4b71711fa2bcc91332dc869db88b01cc4e2
Joe Tavin <jtavin@gmail.com>
2021-07-04 14:47:35 +0300

    implement service B

--- service-b/app/app.py
+++ service-b/app/app.py
@@ -5,18 +5,23 @@ import requests
 import logging, coloredlogs
 
 app = Flask(__name__, static_url_path='')
+coloredlogs.install(level='DEBUG', isatty=True)
+logger = logging.getLogger('app_log')
+
+def redirect_to_service_a():
+    will_redirect = "Hi, I'm Service B! redirecting to service A."
+    logger.debug(will_redirect)
+    # TODO implement redirect to service here
+    return()
 
 @app.route('/')
-def echo_is_alive():
-    service_name = 'Service B'
-    return f'{service_name} is Alive!'
+@app.route('/<request>')
+def echo_nothing_here(request):
+    if request != 'rate':
+        return 'nothing here'
+    else:
+        return(redirect_to_service_a())
 
-@app.route('/price')
-def get_price():
-    api_endpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-    response = requests.get(api_endpoint)
-    price = response.json()
-    return(price)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
